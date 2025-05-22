9c60d13b8844833006f2880b67a9d80ad9511f8c
Daniel-Psouza <danielponcesouzagpi@gmail.com>
2025-05-22 17:06:31 -0300

    Refactor application layout and routing
    - Removed inline styles for background color from app.blade.php.
    - Changed the home route to render the PgEntrada component instead of Welcome.
    - Added new PgEntrada.vue component for the landing page with a login button and title for the medical clinic management system.

---
+++ resources/js/pages/PgEntrada.vue
@@ -0,0 +1,22 @@
+<script setup lang='ts'>
+import { router } from '@inertiajs/vue3';
+
+function goToLogin() {
+    router.visit(route('login'));
+}
+</script>
+
+<template>
+    <div class="bg-blue-500 text-white h-screen flex flex-col">
+        <div class="flex justify-end pr-8 mt-8">
+            <button class="bg-blue-900 text-white p-2 rounded-lg" @click="goToLogin">
+                Login
+            </button>
+        </div>
+        <div class="text-center mt-28 text-xl">
+            <h1 class="inline-block mx-auto bg-blue-900 p-4 rounded-lg ">
+                Sistema de Gerenciamento de Clínicas Médicas</h1>
+        </div>
+    </div>
+
+</template>
